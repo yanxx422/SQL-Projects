3dc3a25f108b0f29d27c5e309785d31c17853021
yanxx422 <yanxx401@umn.edu>
2021-08-02 17:36:09 -0400

    Update Udiddit.sql

--- Udiddit.sql
+++ Udiddit.sql
@@ -47,3 +47,70 @@ CREATE TABLE comments (
 );
 
 CREATE INDEX "find_parent_by_children" ON "comments" ("parent_comment_id");
+
+
+
+
+
+
+INSERT INTO "topics" ("topic_name")
+	SELECT DISTINCT "topic" FROM "bad_posts";
+
+
+    
+INSERT INTO "users" ("user_name")
+	SELECT DISTINCT "username" FROM "bad_posts";
+    
+INSERT INTO "users" ("user_name")    
+    SELECT DISTINCT "user_name" FROM "bad_comments" 
+    WHERE 
+    NOT EXISTS (SELECT "user_name" 
+                FROM users);
+    
+    
+
+    
+INSERT INTO "users" ("user_name")  
+    SELECT DISTINCT REGEXP_SPLIT_TO_TABLE(downvotes,',') FROM "bad_posts"
+    WHERE 
+    NOT EXISTS (SELECT "user_name" 
+                FROM users);
+    
+INSERT INTO "users" ("user_name")      
+    SELECT DISTINCT REGEXP_SPLIT_TO_TABLE(upvotes,',') FROM "bad_posts"
+    WHERE 
+    NOT EXISTS (SELECT "user_name" 
+                FROM users);
+                
+INSERT INTO "posts" ("title")
+	SELECT "title" FROM "bad_posts";
+
+INSERT INTO "posts" ("url")
+	SELECT "url" FROM "bad_posts";
+
+INSERT INTO "posts" ("text_content")
+	SELECT "text_content" FROM "bad_posts";
+    
+UPDATE "comments" SET "post_id" = (
+  SELECT post_id 
+  FROM bad_comments
+  WHERE bad_comments.id = comments.id
+);
+
+UPDATE "comments" SET "user_id" = (
+  SELECT id 
+  FROM users
+  WHERE bad_comments.username = users.username
+);
+
+INSERT INTO "votes" ("post_id","user_id") 
+	SELECT bad_posts.id, users.id from bad_posts JOIN usrs ON users.user_name = bad_posts.REGEXP_SPLIT_TO_TABLE(upvotes,',');
+
+UPDATE "votes" SET vote_value = 1;
+
+INSERT INTO "votes" ("post_id","user_id") 
+	SELECT bad_posts.id, users.id from bad_posts JOIN usrs ON users.user_name = bad_posts.REGEXP_SPLIT_TO_TABLE(downvotes,',');
+
+UPDATE "votes" SET vote_value = -1 WHERE vote_value is NULL;
+
+	
