d34882cd1856dac97eae5fade896fef25b9f6952
yanxx422 <yanxx401@umn.edu>
2021-08-03 11:46:54 -0400

    Update Udiddit.sql

--- Udiddit.sql
+++ Udiddit.sql
@@ -82,46 +82,48 @@ INSERT INTO "users" ("user_name")
     NOT EXISTS (SELECT "user_name" 
                 FROM users);
                 
-INSERT INTO "posts" ("title","url","text_content")
-	SELECT "title","url", "text_content" FROM "bad_posts"
-	WHERE LENGTH("title") <= 100 ;
+INSERT INTO "posts" ("title","url","text_content","user_id","topic_id")
+	SELECT bad_posts.title,bad_posts.url, bad_posts.text_content,users.id,topics.id FROM bad_posts
+	JOIN users ON bad_posts.username = users.user_name
+	JOIN topics ON topics.topic_name = bad_posts.topic
+	WHERE LENGTH(bad_posts.title) <= 100 AND length(bad_posts.title) > 0 ;
 
 
 
+INSERT INTO "comments" ("text_content","user_id","post_id")
+	SELECT bad_comments.text_content,users.id,posts.id FROM bad_comments 
+	JOIN users ON bad_comments.username = users.user_name
+	JOIN posts ON posts.id = bad_comments.post_id;
+
+CREATE VIEW names AS
+	SELECT REGEXP_SPLIT_TO_TABLE(bad_posts.upvotes,',') FROM bad_posts;
+	
+INSERT INTO "votes" ("user_id") 
+	SELECT id from users JOIN names ON users.user_name = names.regexp_split_to_table;
 
 
-
-
-
-
-
-
-
-
-    
-UPDATE "comments" SET "post_id" = (
-  SELECT post_id 
-  FROM bad_comments
-  WHERE bad_comments.id = comments.id
-);
-
-INSERT INTO "comments" ("post_id")
-	SELECT bad_comments.post_id FROM bad_comments JOIN comments ON bad_comments.id = comments.id;
-
-UPDATE "comments" SET "user_id" = (
-  SELECT id 
-  FROM users
-  WHERE bad_comments.username = users.username
-);
-
+INSERT INTO "votes" ("post_id")
+	SELECT posts.id from posts JOIN votes ON posts.user_id = votes.user_id;
+	
+	
+		
+	
 INSERT INTO "votes" ("post_id","user_id") 
-	SELECT bad_posts.id, users.id from bad_posts JOIN users ON users.user_name = bad_posts.REGEXP_SPLIT_TO_TABLE(upvotes,',');
+	SELECT bad_posts.id, users.id from bad_posts INNER JOIN users ON users.user_name = (SELECT REGEXP_SPLIT_TO_TABLE(bad_posts.upvotes,',') FROM bad_posts );
 
 UPDATE "votes" SET vote_value = 1;
 
-INSERT INTO "votes" ("post_id","user_id") 
-	SELECT bad_posts.id, users.id from bad_posts JOIN usrs ON users.user_name = bad_posts.REGEXP_SPLIT_TO_TABLE(downvotes,',');
+DROP VIEW names;
+
+
+CREATE VIEW names AS
+	SELECT REGEXP_SPLIT_TO_TABLE(bad_posts.downvotes,',') FROM bad_posts;
+	
+INSERT INTO "votes" ("user_id") 
+	SELECT id from users JOIN names ON users.user_name = names.regexp_split_to_table;
 
 UPDATE "votes" SET vote_value = -1 WHERE vote_value is NULL;
 
-	
+DROP VIEW names;	
+DROP TABLE bad_comments;
+DROP TABLE bad_posts;
